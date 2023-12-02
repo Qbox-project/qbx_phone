d797c2cfc1c77dfd4d3e2a567452d55028c7b9bc
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-12-02 18:09:06 -0500

    refactor: properly utilize utils (#15)

--- client/animation.lua
+++ client/animation.lua
@@ -1,20 +1,12 @@
 local phoneProp = 0
 local phoneModel = `prop_npc_phone_02`
 
-local function LoadAnimation(dict)
-	RequestAnimDict(dict)
-	while not HasAnimDictLoaded(dict) do
-		Wait(1)
-	end
-end
-
 local function CheckAnimLoop()
     CreateThread(function()
         while PhoneData.AnimationData.lib ~= nil and PhoneData.AnimationData.anim ~= nil do
-            local ped = PlayerPedId()
-            if not IsEntityPlayingAnim(ped, PhoneData.AnimationData.lib, PhoneData.AnimationData.anim, 3) then
-                LoadAnimation(PhoneData.AnimationData.lib)
-                TaskPlayAnim(ped, PhoneData.AnimationData.lib, PhoneData.AnimationData.anim, 3.0, 3.0, -1, 50, 0, false, false, false)
+            if not IsEntityPlayingAnim(cache.ped, PhoneData.AnimationData.lib, PhoneData.AnimationData.anim, 3) then
+                lib.requestAnimDict(PhoneData.AnimationData.lib)
+                TaskPlayAnim(cache.ped, PhoneData.AnimationData.lib, PhoneData.AnimationData.anim, 3.0, 3.0, -1, 50, 0, false, false, false)
             end
             Wait(500)
         end
@@ -23,17 +15,14 @@ end
 
 function newPhoneProp()
 	deletePhone()
-	RequestModel(phoneModel)
-	while not HasModelLoaded(phoneModel) do
-		Wait(1)
-	end
-	phoneProp = CreateObject(phoneModel, 1.0, 1.0, 1.0, 1, 1, 0)
+	lib.requestModel(phoneModel)
+	phoneProp = CreateObject(phoneModel, 1.0, 1.0, 1.0, true, true, false)
 
-	local bone = GetPedBoneIndex(PlayerPedId(), 28422)
+	local bone = GetPedBoneIndex(cache.ped, 28422)
 	if phoneModel == `prop_cs_phone_01` then
-		AttachEntityToEntity(phoneProp, PlayerPedId(), bone, 0.0, 0.0, 0.0, 50.0, 320.0, 50.0, 1, 1, 0, 0, 2, 1)
+		AttachEntityToEntity(phoneProp, cache.ped, bone, 0.0, 0.0, 0.0, 50.0, 320.0, 50.0, 1, 1, 0, 0, 2, 1)
 	else
-		AttachEntityToEntity(phoneProp, PlayerPedId(), bone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0, 0, 2, 1)
+		AttachEntityToEntity(phoneProp, cache.ped, bone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0, 0, 2, 1)
 	end
 end
 
@@ -45,14 +34,13 @@ function deletePhone()
 end
 
 function DoPhoneAnimation(anim)
-    local ped = PlayerPedId()
     local AnimationLib = 'cellphone@'
     local AnimationStatus = anim
-    if IsPedInAnyVehicle(ped, false) then
+    if IsPedInAnyVehicle(cache.ped, false) then
         AnimationLib = 'anim@cellphone@in_car@ps'
     end
-    LoadAnimation(AnimationLib)
-    TaskPlayAnim(ped, AnimationLib, AnimationStatus, 3.0, 3.0, -1, 50, 0, false, false, false)
+    lib.requestAnimDict(AnimationLib)
+    TaskPlayAnim(cache.ped, AnimationLib, AnimationStatus, 3.0, 3.0, -1, 50, 0, false, false, false)
     PhoneData.AnimationData.lib = AnimationLib
     PhoneData.AnimationData.anim = AnimationStatus
     CheckAnimLoop()
